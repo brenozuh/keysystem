9bfd1ea754287fedb6a636a198d6ac9e3dfc78ee
fortniteskibiditoilet <Brenoscaramelo1@gmail.com>
2025-12-21 21:31:43 -0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,1 +1,11 @@
-67towerdefense
+local chars = {
+    54, 55, 116, 111, 119, 101, 114,
+    100, 101, 102, 101, 110, 115, 101
+}
+
+local key = ""
+for i = 1, #chars do
+    key = key .. string.char(chars[i])
+end
+
+return key
